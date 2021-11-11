605cbc4f14d0447f61c50458bf6471e3f47d992c
jedid26 <josedavidbermejopadilla@hotmail.com>
2021-11-11 17:56:39 -0500

    inicio -clase 1: creación repo -clase 2: tipos de datos y operadores

---
+++ pruebas/src/main/java/pruebas/TiposDeDatos.java
@@ -0,0 +1,41 @@
+package pruebas;
+
+public class TiposDeDatos {
+	
+	// argumentos o parametros que "vienen" desde afuera
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// comentario
+		
+		// tipos de datos
+		// edad > numérico
+		// byte < short < int < long < float < double
+		
+		byte edad = 10;
+		short edadShort = 10;
+		int edadInt = 10;
+		long edadLong = 10;
+		float  edadFloat = 10;
+		double edadDouble = 10;
+		
+		// edadInt = edadFloat; no se puede hacer
+		
+		//definir
+		String nombre;
+		
+		//asignar
+		nombre = "algo";
+		
+		boolean flag = true;
+		boolean otroFlag = false;
+		
+		char letra = 'a';
+		
+		String apellido = new String("lope<");
+		String apellido2 = "lopez";
+		
+		//operadores
+		
+	}
+
+}
