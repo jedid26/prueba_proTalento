2ff425719e7055026dbe97b2a25bd98341286902
jedid26 <josedavidbermejopadilla@hotmail.com>
2021-12-15 17:00:12 -0500

    tp 4 completa

--- pruebas_tp/src/main/java/torre_control/Ovni.java
+++ pruebas_tp/src/main/java/torre_control/Ovni.java
@@ -1,5 +1,10 @@
 package torre_control;
 
 public class Ovni extends Voladores {
+
+	public Ovni(String nombre, boolean esOrganico) {
+		super(nombre, esOrganico);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
