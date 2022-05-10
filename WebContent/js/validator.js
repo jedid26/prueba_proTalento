b377093fbd3c7225dbde7f29af3af56e1733a01a
jedid26 <josedavidbermejopadilla@hotmail.com>
2022-05-10 14:12:40 -0500

    Avance 2
    Validaciones completas

--- WebContent/js/validator.js
+++ WebContent/js/validator.js
@@ -57,7 +57,78 @@ function validarEmail(){
 	return true;
 }
 
+function validarCheck(){
+	let check = document.getElementById("validarCondiciones");
+	
+	let feedBack = document.getElementById("inputCondicionesFeedback");
+	
+	//check.
+	
+	if (check){
+		feedBack.classList.remove("valid-feedback");
+		feedBack.classList.add("invalid-feedback");
+		feedBack.innerHTML = "Debe aceptar los terminos y condiciones";
+		return false;
+	}
+	feedBack.classList.remove("invalid-feedback");
+	feedBack.classList.add("valid-feedback");
+	feedBack.innerHTML = "Terminos y condiciones aceptados";
+	return true;
+}
 
+function validarEdad() {
+	//Tag input para manejar clase
+	let input = document.getElementById("selectInput");
+	
+	//Tag div feedback
+	let feedBack = document.getElementById("inputCondicionesFeedback");
+	
+	if (input.selectedIndex === 0){
+		inputClass.classList.remove("is-valid")
+		feedBack.classList.remove("valid-feedback");
+		inputClass.classList.add("is-invalid");
+		feedBack.classList.add("invalid-feedback");
+		feedBack.innerHTML = "Indicar si es mayor o menor de edad";
+		return false;
+	}
+	input.classList.remove("is-invalid");
+	feedBack.classList.remove("invalid-feedback");
+	input.classList.add("is-valid");
+	feedBack.classList.add("valid-feedback");
+	feedBack.innerHTML = "Indicado";
+	return true;
+}
 
+function validarRadio() {
+	
+	let opt1 = document.getElementById("radioBackendInput");
+	let opt2 = document.getElementById("radioFrontendInput");
+	let opt3 = document.getElementById("radioFullstackInput");
+	
+	let feedBack = document.getElementById("inputRadioFeedback");
+	
+	if(!opt1.checked || !opt2.checked || !opt3.checked){
+		feedBack.classList.remove("is-valid");
+		feedBack.classList.add("is-invalid")
+		feedBack.innerHTML = "Seleccione uno";
+		return false;
+	}
+	feedBack.classList.remove("is-invalid");
+	feedBack.classList.add("is-valid");
+	return true;
+}
 
+function validarForm() {
+	
+	let valNombre = validarName();
+	let valEmail = validarEmail();
+	let valCheck = validarCheck();
+	let valEdad = validarEdad();
+	let valRadio = validarRadio();
+	
+	if (!valCheck || !valEdad || !valRadio || !valNombre || !valEmail){		
+		event.preventDefault();
+	}
+		
+}
 
